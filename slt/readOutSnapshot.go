4121c03e22ff690423a794007bcb43f3917c3dd6
brunetto@spritz <brunetto.ziosi@gmail.com>
2014-01-30 16:35:57 +0100

    ripuliti alcuni commenti di test, tolto il messaggio sul timestep corrotto dopo l'ultimo integro, eliminato il newline all'inizio dell'output che faceva crashare SL con segfault

--- slt/readOutSnapshot.go
+++ slt/readOutSnapshot.go
@@ -3,7 +3,7 @@ package slt
 import (
 	"bitbucket.org/brunetto/goutils/readfile"
 	"bufio"
-	"fmt"
+// 	"fmt"
 	"log"
 	"regexp"
 	"strings"
@@ -36,8 +36,8 @@ func ReadOutSnapshot(nReader *bufio.Reader) (*OutSnapshot, error) {
 		regSysTime = regexp.MustCompile(`system_time\s*=\s*(\d+)`)
 		resSysTime []string
 		// This variables are the idxs to print the last or last 10 lines
-		dataStartIdx int = 0
-		dataEndIdx int
+// 		dataStartIdx int = 0
+// 		dataEndIdx int
 	)
 
 	// Init snapshot container
@@ -48,22 +48,6 @@ func ReadOutSnapshot(nReader *bufio.Reader) (*OutSnapshot, error) {
 	for {
 		// Read line by line
 		if line, err = readfile.Readln(nReader); err != nil {
-			if err.Error() == "EOF" {
-				log.Println("File reading complete...")
-				log.Println("Timestep not complete.")
-				log.Println("Last ten lines:")
-				dataEndIdx = len(snap.Data)-1
-				
-				// Check that we have more than 10 lines
-				if dataEndIdx > 10 {
-					dataStartIdx = dataEndIdx - 10
-				}
-				for idx, row := range snap.Data[dataStartIdx:dataEndIdx] {
-					fmt.Println(idx, ": ", row)
-				}
-			} else {
-				log.Fatal("Non EOF error while reading ", err)
-			}
 			// Mark snapshot as corrupted
 			snap.Integrity = false
 			return snap, err
@@ -75,7 +59,6 @@ func ReadOutSnapshot(nReader *bufio.Reader) (*OutSnapshot, error) {
 		// Search for timestep number
 		if resSysTime = regSysTime.FindStringSubmatch(line); resSysTime != nil {
 			snap.Timestep = resSysTime[1]
-// 			log.Println("Reading timestep ", resSysTime[1])
 		}
 		
 		// Check if entering or exiting a particle
@@ -89,9 +72,8 @@ func ReadOutSnapshot(nReader *bufio.Reader) (*OutSnapshot, error) {
 		// Check whether the whole snapshot is in memory
 		// (root particle complete) and if true, return
 		if snap.NestingLevel == 0 {
-// 			log.Println("Timestep ended with nesting: ", snap.NestingLevel)
 			snap.Integrity = true
-			log.Println("Timestep ", snap.Timestep, " integrity set to: ", snap.Integrity)
+			log.Print("Timestep ", snap.Timestep, " integrity set to: ", snap.Integrity)
 			return snap, err
 		}
 	}	
